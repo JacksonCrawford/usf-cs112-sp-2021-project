825fdb919cb46a6937b789aac742e04d8bf63d12
JacksonCrawford <jacksoncrawford@protonmail.com>
2021-03-29 21:56:25 -0400

    Update class

--- DataAnalytics/DataPoint.java
+++ DataAnalytics/DataPoint.java
@@ -3,6 +3,17 @@ public class DataPoint {
     public double f2;
     public String label;
     public boolean isTest;
+    
+    // Constructor
+    public DataPoint(double f1Param, double f2Param, String lParam, boolean tParam) {
+    	this.f1 = f1Param;
+    	this.f2 = f2Param;
+    	this.isTest = tParam;
+    	// Only set label if point is not a test point
+    	if(!this.isTest) {
+    		this.label = lParam;
+    	}
+    }
 
     // Default Constructor
     public DataPoint() {
@@ -13,38 +24,61 @@ public class DataPoint {
     }
 
 
-    // Accessors
+    /*
+     * Accessors
+     */
+    
+    // Returns f1 value (dbl)
     public double getf1() {
         return f1;
     }
 
+    // Returns f1 value (dbl)
     public double getf2() {
         return f2;
     }
 
+    // Returns label value (str)
     public String getLabel() {
         return label;
     }
 
+    // Returns isTest value (bool)
     public boolean getIsTest() {
         return isTest;
     }
 
 
-    // Mutators
+    /*
+     * Mutators
+     */
+    
+    // Sets f1 value (dbl)
     public void setf1(double fParam) {
         f1 = fParam;
     }
 
+    // Sets f2 value (dbl)
     public void setf2(double fParam) {
         f2 = fParam;
     }
 
+    // Sets label (str)
     public void setLabel(String lParam) {
-        label = lParam;
+    	// If it is a test point, it does not gets set to null
+    	if(!this.isTest) {
+	        if(! (lParam.equals("green") || lParam.equals("blue"))) {
+	        	return;
+	        } else {
+	        	label = lParam;
+	        }
+    	} else {
+    		this.label = null;
+    	}
     }
 
-    public void setIsTest(tParam) {
+    // Sets isTest value (bool)
+    public void setIsTest(boolean tParam) {
         isTest = tParam;
     }
 
